3c99264c62faf7b25cff327e0523cfa866f668af
Jongmin Kim <blue1992256@mscbrain.com>
2022-08-10 17:57:51 +0900

    Remember-me 인증 구현

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,12 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -19,6 +21,9 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserDetailsService userDetailsService;
 
 
   @Override
@@ -73,6 +78,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           }
         })
         .deleteCookies("remember-me");
-
+      http
+          .rememberMe()
+          .rememberMeParameter("remember-me") // default: remember-me, checkbox 등의 이름과 맞춰야함
+          .tokenValiditySeconds(3600) // 쿠키의 만료시간 설정(초), default: 14일
+          .alwaysRemember(false) // 사용자가 체크박스를 활성화하지 않아도 항상 실행, default: false
+          .userDetailsService(userDetailsService);
   }
 }
